be53a58b6f3ae96cf9bac85bfa80ff1ae18ac871
Tejas Chougale <92290670+Tejas-2107@users.noreply.github.com>
2023-10-25 10:09:21 +0530

    Update Info.js

--- src/pages/User/info/Info.js
+++ src/pages/User/info/Info.js
@@ -4,7 +4,9 @@ function Info() {
     return ( 
         <div className="info">
          <h3>
-         The sustem provides funding, support, and visibility for engineering projects in the following four core categories of community improvement; Access and Abilities, Human Services, Environment, and Education.
+         The sustem provides funding, support, and visibility for engineering projects in 
+         the following four core categories of community improvement; Access and Abilities,
+          Human Services, Environment, and Education.
          </h3>
         </div>
      );
